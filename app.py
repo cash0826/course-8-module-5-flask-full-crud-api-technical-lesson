a7411e51fb262628d2c23cd6c56310c958f9f14c
Melanie Rodriguez <melarodrigg@gmail.com>
2026-03-14 14:23:20 -0600

    Completed technical lesson

--- app.py
+++ app.py
@@ -2,5 +2,60 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+class Event:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+        
+    def to_dict(self):
+        return {"id": self.id, "title": self.title}
+ 
+events = [
+    Event(1, "Tech Meetup"),
+    Event(2, "Python Workshop")
+]
+
+# GET /events
+@app.route("/events", methods=["GET"])
+def get_events():
+    return jsonify(([event.to_dict() for event in events]), 200)
+
+# GET /events/<id>
+@app.route("/events/<int:id>")
+def get_event(id):
+    event = next((e for e in events if e.id == id), None)
+    return jsonify(event.to_dict()) if event else ("Event not found", 404)
+
+# POST /events
+@app.route('/events', methods=["POST"])
+def create_event():
+    data = request.get_json()
+    new_id = max((e.id for e in events), default=0) +1
+    new_event = Event(id=new_id, title=data["title"])
+    events.append(new_event)
+    return jsonify(new_event.to_dict()), 201
+
+# PATCH /events/<id>, edit a given event
+@app.route('/events/<intid>', methods=["PATCH"])
+def update_event(id):
+    data = request.get_json()
+    event = next((e for e in events if e.id == id), None)
+    if not event:
+        return ("Event not found", 404)
+    if "title" in data:
+        event.title = data["title"]
+    return jsonify(event.to_dict())
+
+# DELETE /events/<id>, delete a given event
+@app.route('/events/<int:id>', methods=["DELETE"])
+def delete_event(id):
+    global events
+    event = next((e for e in events if e.id == id), None)
+    if not event:
+        return ("Event not found", 404)
+    events = [e for e in events if e.id != id]
+    return "", 204 # No Content but operation was successful
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
